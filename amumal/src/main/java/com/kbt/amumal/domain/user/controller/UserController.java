ae2cfa1be6c76ee625b829554d90bfb61138614d
songwol <amber_lucy@naver.com>
2026-05-31 03:07:31 +0900

    fix : 예외처리 추가

--- amumal/src/main/java/com/kbt/amumal/domain/user/controller/UserController.java
+++ amumal/src/main/java/com/kbt/amumal/domain/user/controller/UserController.java
@@ -29,6 +29,11 @@ public class UserController {
 
     @GetMapping("/")
     public ApiResponse<UserResDTO.userInfoRes> getUser(@RequestHeader("Authorization") String authorization) {
+        // 헤더가 없거나 잘못된 요청일 때
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            throw new CustomException(ErrorCode.BAD_REQUEST, "인증 토큰이 없습니다.");
+        }
+
         // "Bearer <token>" 에서 토큰 추출
         String token = authorization.replace("Bearer ", "");
 

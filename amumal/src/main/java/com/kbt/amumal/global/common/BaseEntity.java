719a62fc7b9934d16553539c3137a3f51e4ddfa7
songwol <amber_lucy@naver.com>
2026-06-02 17:39:35 +0900

    feat : BaseEntity 구현

--- amumal/src/main/java/com/kbt/amumal/global/common/BaseEntity.java
+++ amumal/src/main/java/com/kbt/amumal/global/common/BaseEntity.java
@@ -1,13 +1,18 @@
 package com.kbt.amumal.global.common;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreatedDate // 저장 시 자동 입력
     @Column(name = "created_at", updatable = false, nullable = false)
@@ -19,4 +24,9 @@ public class BaseEntity {
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
+
+    // patch 삭제
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
 }

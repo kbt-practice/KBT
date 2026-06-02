056a46d9e245f421d4c50dde4d065fa6fd2103e0
songwol <amber_lucy@naver.com>
2026-06-02 17:39:59 +0900

    feat : 토큰에서 유저ID 추출 분리

---
+++ amumal/src/main/java/com/kbt/amumal/global/common/UserIdToken.java
@@ -0,0 +1,25 @@
+package com.kbt.amumal.global.common;
+
+import com.kbt.amumal.global.error.CustomException;
+import com.kbt.amumal.global.error.ErrorCode;
+import com.kbt.amumal.global.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserIdToken {
+    private final JwtUtil jwtUtil;
+
+    public String getUserIdByToken(String authorization) {   // ← public
+        if (authorization == null || !authorization.startsWith("Bearer "))
+            throw new CustomException(ErrorCode.BAD_REQUEST, "유저 정보를 확인해주세요.");
+
+        String token = authorization.replace("Bearer ", "");
+
+        if (!jwtUtil.validateToken(token))
+            throw new CustomException(ErrorCode.UNAUTHORIZED, "유저 정보를 확인해주세요.");
+
+        return jwtUtil.getUserId(token);
+    }
+}

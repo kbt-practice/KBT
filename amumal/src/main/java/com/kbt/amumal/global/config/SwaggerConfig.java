96a6534d0c204f7eb1b4f0267154863c661c75b3
songwol <amber_lucy@naver.com>
2026-06-12 22:59:56 +0900

    fix : 스웨거 수정

--- amumal/src/main/java/com/kbt/amumal/global/config/SwaggerConfig.java
+++ amumal/src/main/java/com/kbt/amumal/global/config/SwaggerConfig.java
@@ -6,12 +6,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${server.url:http://localhost:8080}")
+    private String serverUrl;
 
     static {
         // @LoginUserId 파라미터는 인터셉터가 처리하므로 Swagger 문서에서 숨김
@@ -25,6 +32,10 @@ public class SwaggerConfig {
                         .title("아무말 API")
                         .description("아무말 커뮤니티 서비스 REST API 문서")
                         .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url(serverUrl).description("운영 서버"),
+                        new Server().url("http://localhost:8080").description("로컬 테스트")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth",

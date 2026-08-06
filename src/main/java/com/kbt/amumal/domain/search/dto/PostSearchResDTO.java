2b858d6e6558076866e6ce103ef2f6a850fb04fb
songwol <amber_lucy@naver.com>
2026-08-06 16:43:08 +0900

    feat : 검색/정렬 API 구현

---
+++ src/main/java/com/kbt/amumal/domain/search/dto/PostSearchResDTO.java
@@ -0,0 +1,18 @@
+package com.kbt.amumal.domain.search.dto;
+
+import com.kbt.amumal.domain.post.dto.PostResDTO;
+
+import java.util.List;
+
+public class PostSearchResDTO {
+
+    public record response(
+            List<PostResDTO.postListItem> posts,
+            pagination pagination
+    ) {}
+
+    public record pagination(
+            String nextCursor,
+            boolean hasNext
+    ) {}
+}
---
+++ src/main/java/com/kbt/amumal/domain/search/dto/SearchSortType.java
@@ -0,0 +1,6 @@
+package com.kbt.amumal.domain.search.dto;
+
+public enum SearchSortType {
+    ACCURACY,
+    LATEST
+}
